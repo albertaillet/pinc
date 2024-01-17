3570726a78f895188042077992fbfc2c6db62ac5
Albert Sund Aillet <albert@sundaillet.com>
2024-01-17 23:28:14 +0100

    split main.py into data.py and visualize.py and renamed mlp.py to model.py

--- pinc/data.py
+++ pinc/data.py
@@ -10,3 +10,13 @@ def process_point_cloud(point_cloud: o3d.geometry.PointCloud) -> tuple[np.ndarra
     points = points / max_coord
     assert points.shape[1] == 3
     return points, max_coord, center_point
+
+
+if __name__ == "__main__":
+    from pathlib import Path
+
+    repo_root = Path(__file__).resolve().parent.parent
+    ply_file = repo_root / "data/scenes/gargoyle.ply"
+    point_cloud = o3d.io.read_point_cloud(str(ply_file))
+    points, max_coord, center_point = process_point_cloud(point_cloud)
+    print(points.shape, max_coord, center_point)
