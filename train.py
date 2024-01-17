930a29debd2d7557d2f2104a203715ccc2ed5317
Zigolox <41542666+Zigolox@users.noreply.github.com>
2024-01-17 22:40:19 +0100

    renamed variable names

--- train.py
+++ train.py
@@ -9,7 +9,7 @@ from functools import partial
 
 def step(
     params: Params,
-    data_points: Array,
+    boundary_points: Array,
     sample_points,
     opt_state: OptState,
     optim: GradientTransformation,
@@ -18,15 +18,17 @@ def step(
 ) -> tuple[Params, Array]:
     """Compute loss and update parameters"""
 
-    def batch_loss(params: Params, data_points: Array, sample_points: Array) -> Array:
+    def batch_loss(params: Params, boundary_points: Array, sample_points: Array) -> Array:
         gen_loss = partial(
             compute_loss, params=params, activation=beta_softplus, F=F, skip_layers=skip_layers, loss_weights=loss_weights
         )
         boundary_loss = vmap(partial(gen_loss, boundary=True))
         sample_loss = vmap(partial(gen_loss, boundary=False))
-        return boundary_loss(x=data_points).sum() + sample_loss(x=sample_points).sum() / (len(data_points) + len(sample_points))
+        return boundary_loss(x=boundary_points).sum() + sample_loss(x=sample_points).sum() / (
+            len(boundary_points) + len(sample_points)
+        )
 
-    loss, grad = value_and_grad(batch_loss)(params, data_points, sample_points)
+    loss, grad = value_and_grad(batch_loss)(params, boundary_points, sample_points)
     updates, opt_state = optim.update(grad, opt_state)
     params = apply_updates(params, updates)  # type: ignore
     return params, loss
@@ -48,13 +50,15 @@ def sample_local_points(data_points: Array, std: Array, batch_size: int, key: Ar
     return data_points + normal(key, (batch_size, 3)) * std
 
 
-def get_data(data: Array, data_std: Array, data_batch_size: int, global_batch_size: int, key: Array) -> tuple[Array, Array]:
+def get_batch(data: Array, data_std: Array, data_batch_size: int, global_batch_size: int, key: Array) -> tuple[Array, Array]:
     """Get batch of data"""
     data_key, local_key, global_key = split(key, 3)
-    data_points, std = sample_data(data, data_std, data_batch_size, data_key)
-    local_points = sample_local_points(data_points, std, data_batch_size, local_key)
+
+    boundary_points, std = sample_data(data, data_std, data_batch_size, data_key)
+    local_points = sample_local_points(boundary_points, std, data_batch_size, local_key)
     global_points = sample_global_points(global_batch_size, global_key)
-    return data_points, np.concatenate([local_points, global_points], axis=0)
+
+    return boundary_points, np.concatenate([local_points, global_points], axis=0)
 
 
 def train(
@@ -73,8 +77,8 @@ def train(
 
     def scan_fn(carry: tuple[Params, OptState], key) -> tuple[tuple[Params, OptState], Array]:
         params, opt_state = carry
-        data_points, sample_points = get_data(data, data_std, data_batch_size, global_batch_size, key)
-        params, loss = step(params, data_points, sample_points, opt_state, optim, skip_layers, loss_weights)
+        boundary_points, sample_points = get_batch(data, data_std, data_batch_size, global_batch_size, key)
+        params, loss = step(params, boundary_points, sample_points, opt_state, optim, skip_layers, loss_weights)
         return (params, opt_state), loss
 
     (params, _), loss = scan(scan_fn, (params, optim.init(params)), split(key, num_steps))
@@ -90,9 +94,11 @@ if __name__ == "__main__":
     loss_weights = np.array([1, 0.1, 0.0001, 0.0005, 0.1])
     optim = adam(piecewise_constant_schedule(1e-3, {2000 * i: 0.99 for i in range(1, num_steps // 2000 + 1)}))
     F = lambda x: x / 3
+
     data = normal(data_key, (100, 3))
+    data = data / np.linalg.norm(data, axis=-1, keepdims=True)
     data_std = np.ones_like(data) * 0.1
-    data /= np.linalg.norm(data, axis=-1, keepdims=True)
+
     params, loss = train(
         params, data, data_std, optim, data_batch_size, global_batch_size, num_steps, skip_layers, loss_weights, train_key
     )
