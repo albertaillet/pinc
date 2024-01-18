55a2d23a638888d0e5541f88710f813d0be5b383
Albert Sund Aillet <albert@sundaillet.com>
2024-01-18 21:55:57 +0100

    Revert mlp.py change from "added boundary loss and inner sample generation" This reverts commit 5ef6671dc41640d3455f89bc8e08f771b165f26a.

--- pinc/model.py
+++ pinc/model.py
@@ -6,6 +6,7 @@ from jax.random import normal, split, key
 from jax import Array
 from collections.abc import Callable
 
+
 Params = list[tuple[Array, Array]]
 
 
@@ -54,9 +55,17 @@ def mlp_forward(params: Params, x: Array, activation: Callable[[Array], Array], 
     return w @ x + b
 
 
-def get_variables(
-    params: Params, x: Array, activation: Callable[[Array], Array], F: Callable[[Array], Array], skip_layers: list[int]
-) -> tuple[Array, ...]:
+def curl_from_jacobian(jacobian: Array) -> Array:
+    return np.array(
+        [
+            jacobian[2, 1] - jacobian[1, 2],
+            jacobian[0, 2] - jacobian[2, 0],
+            jacobian[1, 0] - jacobian[0, 1],
+        ]
+    )
+
+
+def get_variables(params: Params, x: Array, activation: Callable, F: Callable, skip_layers: list[int]) -> tuple[Array, ...]:
     def forward_and_aux(x: Array):
         out = mlp_forward(params, x, activation, skip_layers)
         sdf, phi, phi_tilde = out[0], out[1:4], out[4:7]
@@ -66,25 +75,11 @@ def get_variables(
     # shapes: grad_sdf(3,)  jac_phi(3, 3)  jac_G_tilde(3, 3)
     (grad_sdf, jac_phi, jac_G_tilde), (sdf, G_tilde) = jacfwd(forward_and_aux, has_aux=True)(x)
 
-    # calculate curl of phi using the jacobian
-    curl_phi = np.array(
-        [
-            jac_phi[2, 1] - jac_phi[1, 2],
-            jac_phi[0, 2] - jac_phi[2, 0],
-            jac_phi[1, 0] - jac_phi[0, 1],
-        ]
-    )
+    curl_phi = curl_from_jacobian(jac_phi)
     curl_phi_minus_F = curl_phi - F(x)
     G = curl_phi_minus_F / (np.linalg.norm(curl_phi_minus_F) + 1e-6)  # when p approaches inf
 
-    # calculate curl of G_tilde using the jacobian
-    curl_G_tilde = np.array(
-        [
-            jac_G_tilde[2, 1] - jac_G_tilde[1, 2],
-            jac_G_tilde[0, 2] - jac_G_tilde[2, 0],
-            jac_G_tilde[1, 0] - jac_G_tilde[0, 1],
-        ]
-    )
+    curl_G_tilde = curl_from_jacobian(jac_G_tilde)
 
     return sdf, grad_sdf, G, G_tilde, curl_G_tilde
 
@@ -113,8 +108,7 @@ def compute_loss(
             delta_e(sdf, epsilon) * np.linalg.norm(grad_sdf),  # loss function for area
         ]
     )
-    loss @= loss_weights
-    return loss
+    return loss @ loss_weights
 
 
 if __name__ == "__main__":
@@ -125,7 +119,7 @@ if __name__ == "__main__":
     x = np.arange(3, dtype=np.float32)
     # out = mlp_forward(params, x, activation=nn.relu, skip_layers=skip_layers)
     # assert out.shape == (7,)
-    F = lambda x: x / 3
+    F = lambda x: x / 3  # noqa: E731
     # vars = get_variables(params, x, activation=nn.relu, F=F, skip_layers=skip_layers)
     # print(vars)
     loss = compute_loss(params, x, activation=nn.relu, F=F, skip_layers=skip_layers, loss_weights=loss_weights)
