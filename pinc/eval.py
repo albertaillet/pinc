309819407ea793c20617818e9a52dfbe07adbe18
Albert Sund Aillet <albert@sundaillet.com>
2024-01-20 15:44:25 +0100

    added distance metrics calculation

--- pinc/eval.py
+++ pinc/eval.py
@@ -1,20 +1,11 @@
-from functools import partial, wraps
-from typing import Callable
+from functools import partial
 
 import numpy as np
 from jax import Array, vmap
+from skimage.measure import marching_cubes
 
-from pinc.model import StaticLossArgs
-
-
-def input_to_cpu(func: Callable) -> Callable:
-    @wraps(func)
-    def wrapper(*args, **kwargs):
-        args = [np.array(arg) if isinstance(arg, Array) else arg for arg in args]
-        kwargs = {k: np.array(v) if isinstance(v, Array) else v for k, v in kwargs.items()}
-        return func(*args, **kwargs)
-
-    return wrapper
+from pinc.metrics import chamfer_distance, directed_chamfer_distance, directed_hausdorff_distance, hausdorff_distance
+from pinc.model import Params, StaticLossArgs, get_variables, mlp_forward
 
 
 def normal_consistency(x: Array, y: Array) -> Array:
@@ -22,12 +13,37 @@ def normal_consistency(x: Array, y: Array) -> Array:
     return jnp.mean(jnp.abs(jnp.sum(x * y, axis=1)))
 
 
-def evaluate(points: Array, normals: Array, params: list[tuple[Array, Array]], static: StaticLossArgs) -> Array:
-    """Evaluate the SDF at the given points."""
+def evaluate(points: Array, normals: Array, params: Params, static: StaticLossArgs) -> dict[str, float]:
+    # Calculate the normal consistency
     forward = partial(get_variables, params, activation=static.activation, F=static.F, skip_layers=static.skip_layers)
     (_, _, G, _, _) = vmap(forward)(points)
-    nc = normal_consistency(G, normals)
-    return nc
+    nc = normal_consistency(G, normals).item()
+
+    # Level set extraction
+    forward = partial(mlp_forward, params, activation=static.activation, skip_layers=static.skip_layers)
+    max_pts = 1.5  # TODO: set as arguments
+    resolution = 40
+    coord = jnp.linspace(-max_pts, max_pts, resolution)
+    grid = jnp.stack(jnp.meshgrid(coord, coord, coord), axis=-1).reshape(-1, 3)
+    out = vmap(forward)(grid)
+    values = np.array(out[:, 0]).reshape(resolution, resolution, resolution)
+    level_set_value = max(0.0, values.min())  # TODO: use this or a try except
+    level_set_points, _, _, _ = marching_cubes(values.reshape(resolution, resolution, resolution), level_set_value)
+
+    # Calculate the distance metrics
+    numpy_points = np.array(points)
+    cd = chamfer_distance(level_set_points, numpy_points)
+    dcd = directed_chamfer_distance(level_set_points, numpy_points)
+    hd = hausdorff_distance(level_set_points, numpy_points)
+    dhd = directed_hausdorff_distance(level_set_points, numpy_points)
+
+    return {
+        "normal_consistency": nc,
+        "chamfer_distance": cd,
+        "directed_chamfer_distance": dcd,
+        "hausdorff_distance": hd,
+        "directed_hausdorff_distance": dhd,
+    }
 
 
 if __name__ == "__main__":
@@ -37,7 +53,7 @@ if __name__ == "__main__":
     from jax import nn, random
 
     from pinc.data import load_ply, process_points
-    from pinc.model import get_variables, init_mlp_params
+    from pinc.model import init_mlp_params
 
     skip_layers = [1]
     activation = nn.relu
