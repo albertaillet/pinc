3279fca499202249c1bebaffb10fe8dd984ae6c1
Albert Sund Aillet <albert@sundaillet.com>
2024-01-21 15:39:26 +0100

    move to trimesh for load ply pointcloud

--- pinc/data.py
+++ pinc/data.py
@@ -1,16 +1,20 @@
 from pathlib import Path
 
 import numpy as np
-from plyfile import PlyData
+import trimesh
 from scipy.spatial import cKDTree
 
 
 def load_ply(path: Path) -> tuple[np.ndarray, np.ndarray]:
     """Load point cloud from .ply file."""
     assert path.exists() and path.suffix == ".ply"
-    with open(path, "rb") as f:
-        plydata = PlyData.read(f)
-    data = np.stack([plydata["vertex"][coord] for coord in ["x", "y", "z", "nx", "ny", "nz"]], axis=1)
+    mesh = trimesh.load(path)
+    vertex_metadata = mesh.metadata["_ply_raw"]["vertex"]  # type: ignore
+    expected_dtype = [("x", "<f4"), ("y", "<f4"), ("z", "<f4"), ("nx", "<f4"), ("ny", "<f4"), ("nz", "<f4")]
+    assert vertex_metadata["data"].dtype == expected_dtype
+    # vertex_metadata["data"] has to be converted from an array of tuples
+    data = np.asarray(vertex_metadata["data"].tolist())
+    assert data.shape == (vertex_metadata["length"], 6)
     return data[:, :3], data[:, 3:]  # points, normals
 
 
@@ -32,11 +36,13 @@ def get_sigma(points: np.ndarray, k: int = 50) -> np.ndarray:
 
 if __name__ == "__main__":
     repo_root = Path(__file__).resolve().parent.parent
-    ply_file = repo_root / "data/scans/gargoyle.ply"
-    points, normals = load_ply(ply_file)
-    print(points.shape, normals.shape)
-    points, max_coord, center_point = process_points(points)
-    print(points.shape, max_coord, center_point)
-
-    sigma = get_sigma(points)
-    print(sigma.shape, type(sigma), sigma.dtype)
+    for ply_file in repo_root.glob("data/scans/*.ply"):
+        points, normals = load_ply(ply_file)
+        print(ply_file, points.shape, normals.shape)
+        points, normals = load_ply(ply_file)
+        print(points.shape, normals.shape)
+        points, max_coord, center_point = process_points(points)
+        print(points.shape, max_coord, center_point)
+        sigma = get_sigma(points)
+        print(sigma.shape, type(sigma), sigma.dtype)
+        break
