3bc72e8b4f1b0ad70487a46e3cd6fe8b771eb987
Zigolox <41542666+Zigolox@users.noreply.github.com>
2024-01-23 00:54:22 +0100

    removed util.py

--- pinc/utils.py
+++ pinc/utils.py
@@ -1,8 +1,11 @@
-from typing import Callable
+from collections.abc import Callable
+from functools import wraps
+from typing import Optional
 
 import jax.numpy as jnp
 import numpy as np
-from jax import Array, vmap
+from jax import Array, lax, vmap
+from jax.experimental.host_callback import id_tap
 from skimage.measure import marching_cubes
 
 
@@ -21,3 +24,59 @@ def mesh_from_sdf(sdf: Callable, grid_range: float, resolution: int, level: floa
         return marching_cubes(sd_grid_numpy, level)
     except ValueError:  # no level set crossing
         return np.zeros((1, 3)), np.zeros((1, 3))
+
+
+def scan_eval_log(
+    eval_freq: Optional[int],
+    loss_freq: Optional[int],
+    log_eval: Callable,
+    log_loss: Callable,
+) -> Callable:
+    """Decorator that starts eval logging to `body_fun` used in `jax.lax.scan`."""
+
+    def _scan_eval_log(func: Callable) -> Callable:
+        @wraps(func)
+        def wrapped_log(carry: tuple, x: tuple) -> tuple:
+            iter_num, *_ = x
+            model, *_ = carry
+
+            lax.cond(
+                eval_freq is not None and iter_num % eval_freq == 0,
+                lambda model, iter_num: id_tap(log_eval, (model, iter_num)),
+                lambda *args: args,
+                model,
+                iter_num,
+            )
+            out_carry, loss = func(carry, x)
+
+            lax.cond(
+                loss_freq is not None and iter_num % loss_freq == 0,
+                lambda loss, iter_num: id_tap(log_loss, (loss, iter_num)),
+                lambda *args: args,
+                loss,
+                iter_num,
+            )
+
+            return out_carry, loss
+
+        return wrapped_log
+
+    return _scan_eval_log
+
+
+if __name__ == "__main__":
+    import jax.numpy as jnp
+    import wandb
+
+    wandb.init(project="test")
+
+    @scan_eval_log(
+        eval_freq=None,
+        loss_freq=10,
+        log_eval=lambda x, _: wandb.log({"eval": x[0]}, step=x[1]),
+        log_loss=lambda x, _: wandb.log({"loss": x[0]}, step=x[1]),
+    )
+    def scan_step(carry, x):
+        return (carry[0] + 1,), x[0]
+
+    lax.scan(scan_step, (0,), (jnp.arange(100), jnp.arange(100)))
