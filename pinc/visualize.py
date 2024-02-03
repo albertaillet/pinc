3f888ba1887e0d8e9831fd6d3d1dc088acd3f26c
Albert Sund Aillet <albert@sundaillet.com>
2024-02-03 16:46:27 +0100

    added draft of slice viz

--- pinc/visualize.py
+++ pinc/visualize.py
@@ -1,3 +1,4 @@
+from matplotlib import pyplot as plt
 from plotly import graph_objects as go
 
 
@@ -7,9 +8,10 @@ def figure(trace, title) -> go.Figure:
         layout=go.Layout(
             title=title,
             scene=dict(
-                xaxis=dict(title="X"),
-                yaxis=dict(title="Y"),
-                zaxis=dict(title="Z"),
+                xaxis=dict(title="X", range=[-2, 2], autorange=False),
+                yaxis=dict(title="Y", range=[-2, 2], autorange=False),
+                zaxis=dict(title="Z", range=[-2, 2], autorange=False),
+                aspectratio=dict(x=1, y=1, z=1),
             ),
         ),
     )
@@ -47,8 +49,9 @@ if __name__ == "__main__":
     from functools import partial
 
     import jax.numpy as jnp
+    from jax import vmap
 
-    from pinc.utils import Array, mesh_from_sdf
+    from pinc.utils import Array, mesh_from_sdf  # noqa: F401
 
     def sdf_sphere(point: Array, radius: float) -> Array:
         return jnp.linalg.norm(point) - radius
@@ -57,7 +60,23 @@ if __name__ == "__main__":
         q = jnp.stack([jnp.linalg.norm(point[:2]) - radius, point[2]])
         return jnp.linalg.norm(q) - tube_radius
 
-    sdf = partial(sdf_torus, radius=0.2, tube_radius=0.1)
-    verts, faces, _, _ = mesh_from_sdf(sdf, grid_range=1.5, resolution=40, level=0.0)
-    plot_points(verts, title="Torus").show()
-    plot_mesh(verts, faces, title="Torus").show()
+    sdf = partial(sdf_torus, radius=0.7, tube_radius=0.3)
+    # verts, faces = mesh_from_sdf(sdf, grid_range=1.5, resolution=40, level=0.0)
+    # plot_points(verts, title="Torus").show()
+    # plot_mesh(verts, faces, title="Torus").show()
+
+    def show_slice(sdf, z=0.0, w=200, r=1.1):
+        y, x = jnp.mgrid[-r : r : w * 1j, -r : r : w * 1j].reshape(2, -1)
+        z = z * jnp.ones_like(x)
+        p = jnp.c_[x, y, z]
+        d = vmap(sdf)(p).reshape(w, w)
+        plt.figure(figsize=(5, 5))
+        kw = dict(extent=(-r, r, -r, r), vmin=-r, vmax=r)
+        plt.contourf(d, 16, cmap="bwr", **kw)
+        plt.contour(d, levels=[0.0], colors="black", **kw)
+        plt.axis("equal")
+        plt.xlabel("x")
+        plt.ylabel("y")
+        plt.show()
+
+    show_slice(sdf)
