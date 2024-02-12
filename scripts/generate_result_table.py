faac2875fcc57df3d84e2768e29a05774aae7c53
Albert Sund Aillet <albert@sundaillet.com>
2024-02-12 22:16:34 +0100

    split into functions and added df creation

--- scripts/generate_result_table.py
+++ scripts/generate_result_table.py
@@ -2,6 +2,8 @@ import json
 from enum import StrEnum
 from typing import NamedTuple
 
+import pandas as pd
+
 from pinc.data import REPO_ROOT
 
 
@@ -23,27 +25,9 @@ PINC 0.29 7.54 0.09 1.20 0.37 7.24 0.11 1.88 0.14 2.56 0.04 2.73 0.16 4.78 0.05 
 """
 
 
-def parse_table(table: str, columns: list[str]) -> dict[str, dict[str, Metrics]]:
-    metric_results = {}
-    for line in table.split("\n")[1:-1]:  # skip the first and last empty lines
-        chunks = line.split(" ")
-        key = chunks[0].lower()
-        values = list(map(float, chunks[1:]))
-        metric_results[key] = {f: Metrics(*values[i : i + 4]) for f, i in zip(columns, range(0, len(values), 4))}
-
-    # invert the dictionary to have the filenames as the top-level keys
-    return {f: {k: metric_results[k][f] for k in metric_results} for f in columns}
-
-
-reported_metrics = parse_table(PAPER_TABLE, PAPER_SRB_FILES)
-
-print(json.dumps(reported_metrics))
-
-
 class RunTypes(StrEnum):
     TYPE_1 = "jax with epsilon=0.1"
     TYPE_2 = "jax with epsilon=1.0"
-    TYPE_3 = "pytorch with epsilon=0.1"
 
 
 RUN_IDS = {
@@ -69,48 +53,84 @@ RUN_IDS = {
     },
 }
 
-for file, run_ids in RUN_IDS.items():
-    for spec, run_id in run_ids.items():
-        if run_id is None:
-            continue
-        matching_run_dirs = list((REPO_ROOT / "wandb").glob(f"run-*-{run_id}"))
-        assert len(matching_run_dirs) == 1, f"Found {len(matching_run_dirs)} matching run directories for {run_id}"
-        run_dir = matching_run_dirs[0]
-        assert run_dir.is_dir(), f"{run_dir} is not a directory"
 
-        # load the metrics from the run directory
-        metrics_path = run_dir / "files" / "config.json"
+def parse_table(table: str, columns: list[str]) -> dict[str, dict[str, Metrics]]:
+    """Parses the table string and returns a dictionary of the metrics for each file and method."""
+    metric_results = {}
+    for line in table.split("\n")[1:-1]:  # skip the first and last empty lines
+        chunks = line.split(" ")
+        key = chunks[0].lower()
+        values = list(map(float, chunks[1:]))
+        metric_results[key] = {f: Metrics(*values[i : i + 4]) for f, i in zip(columns, range(0, 20, 4))}
 
-        with (run_dir / "files" / "config.json").open("r") as f:
-            config = json.load(f)
+    # invert the dictionary to have the file names as the keys
+    return {file: {method: metric_results[method][file] for method in metric_results} for file in columns}
 
-        # Check if the config file is correct
-        if spec == RunTypes.TYPE_1:
-            assert config["epsilon"] == 0.1
-        elif spec == RunTypes.TYPE_2:
-            assert config["epsilon"] == 1.0
-        else:
-            raise ValueError(f"Unknown spec: {spec}")
-        assert config["data_filename"] == file
-        assert len(config["loss_weights"]) == 4  # check for the right version of the config
 
-        # load the metrics from the run directory
-        metrics_path = run_dir / "files" / "metrics"
-        key_fun = lambda p: int(p.stem.split("_")[1])
-        metrics = sorted(metrics_path.glob("model_*_metrics.json"), key=key_fun)
-        if len(metrics) == 0:
-            continue
-        metric_path = metrics[-1]
-        assert key_fun(metric_path) == 100_000
-        with metric_path.open("r") as f:
-            metric_data = json.load(f)
-        metric = Metrics(
-            gt_chamfer=metric_data["distances"]["ground_truth"]["chamfer"],
-            gt_hausdorff=metric_data["distances"]["ground_truth"]["hausdorff"],
-            scan_directed_chamfer=metric_data["distances"]["scan"]["directed_chamfer"],
-            scan_directed_hausdorff=metric_data["distances"]["scan"]["directed_hausdorff"],
-        )
+def flatten_metrics_for_df(metrics: dict[str, dict[str, Metrics]]) -> dict[str, dict[tuple[str, str], float]]:
+    # flatten the dictionary to have a tuple of (file, metric_name) as the key and the spec as the subkey
+    flat_metrics = {}
+    for file, specs in metrics.items():
+        for spec, metric in specs.items():
+            for metric_name, value in metric._asdict().items():
+                if (file, metric_name) not in flat_metrics:
+                    flat_metrics[(file, metric_name)] = {}
+                flat_metrics[(file, metric_name)][spec] = value
+    return flat_metrics
 
-        reported_metrics[file][spec] = metric
 
-print(json.dumps(reported_metrics))
+def add_runs_to_reported_metrics(reported_metrics: dict[str, dict[str, Metrics]]) -> dict[str, dict[str, Metrics]]:
+    for file, run_ids in RUN_IDS.items():
+        for spec, run_id in run_ids.items():
+            if run_id is None:
+                continue
+            matching_run_dirs = list((REPO_ROOT / "wandb").glob(f"run-*-{run_id}"))
+            assert len(matching_run_dirs) == 1, f"Found {len(matching_run_dirs)} matching run directories for {run_id}"
+            run_dir = matching_run_dirs[0]
+            assert run_dir.is_dir(), f"{run_dir} is not a directory"
+
+            # load the metrics from the run directory
+            metrics_path = run_dir / "files" / "config.json"
+
+            with (run_dir / "files" / "config.json").open("r") as f:
+                config = json.load(f)
+
+            # Check if the config file is correct
+            if spec == RunTypes.TYPE_1:
+                assert config["epsilon"] == 0.1
+            elif spec == RunTypes.TYPE_2:
+                assert config["epsilon"] == 1.0
+            else:
+                raise ValueError(f"Unknown spec: {spec}")
+            assert config["data_filename"] == file
+            assert len(config["loss_weights"]) == 4  # check for the right version of the config
+
+            # load the metrics from the run directory
+            metrics_path = run_dir / "files" / "metrics"
+            key_fun = lambda p: int(p.stem.split("_")[1])
+            metrics = sorted(metrics_path.glob("model_*_metrics.json"), key=key_fun)
+            if len(metrics) == 0:
+                continue
+            metric_path = metrics[-1]
+            assert key_fun(metric_path) == 100_000
+            with metric_path.open("r") as f:
+                metric_data = json.load(f)
+            metric = Metrics(
+                gt_chamfer=metric_data["distances"]["ground_truth"]["chamfer"],
+                gt_hausdorff=metric_data["distances"]["ground_truth"]["hausdorff"],
+                scan_directed_chamfer=metric_data["distances"]["scan"]["directed_chamfer"],
+                scan_directed_hausdorff=metric_data["distances"]["scan"]["directed_hausdorff"],
+            )
+
+            reported_metrics[file][spec] = metric
+    return reported_metrics
+
+
+if __name__ == "__main__":
+    reported_metrics = parse_table(PAPER_TABLE, PAPER_SRB_FILES)
+
+    reported_metrics = add_runs_to_reported_metrics(reported_metrics)
+
+    flat_metrics = flatten_metrics_for_df(reported_metrics)
+
+    print(pd.DataFrame(flat_metrics))
